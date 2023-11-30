5f4f45be0cfc33cc8b712449f5b1b7a7b1880654
cherryApp <j.cserko@gmail.com>
2023-11-30 09:24:05 +0100

    signalstore

--- src/app/store/CustomerEntityStore.ts
+++ src/app/store/CustomerEntityStore.ts
@@ -1,51 +1,75 @@
-import { computed, inject } from '@angular/core';
-import { signalStore, type, withComputed, withMethods, patchState } from '@ngrx/signals';
+import { Type, computed, effect, inject } from '@angular/core';
+import {
+  signalStore,
+  type,
+  withComputed,
+  withMethods,
+  patchState,
+  withState,
+  withHooks,
+} from '@ngrx/signals';
 import { setAllEntities, withEntities } from '@ngrx/signals/entities';
 import { Customer } from '../model/customer';
 import { CustomerService } from '../service/customer.service';
 import { firstValueFrom } from 'rxjs';
 
-const CustomerEntityStore = signalStore(
+export const CustomerEntityStore = signalStore(
+  { providedIn: 'root' },
+  withState({
+    selectID: 0,
+    initialized: false,
+    loading: false,
+  }),
 
   // Defining an Entity
-  withEntities({ entity: type<Customer>(), collection: 'customers' }),
+  withEntities({
+    entity: type<Customer>(),
+    collection: 'customers',
+  }),
 
   withMethods((state) => {
     const customerService = inject(CustomerService);
 
     return {
       load: async () => {
-        const customers = await firstValueFrom( customerService.getAll() );
-        patchState(state, setAllEntities(customers, { collection: 'customers' }));
+        const customers = await firstValueFrom(customerService.getAll());
+        patchState(
+          state,
+          setAllEntities(customers, { collection: 'customers' })
+        );
       },
-    }
-  })
+    };
+  }),
 
   // withEntities created a customersEntities signal for us:
-  // withComputed(({ customersEntities, basket, from, to }) => ({
-  //   selected: computed(() => customersEntities().filter((c) => basket()[c.id])),
-  //   criteria: computed(() => ({ from: from(), to: to() })),
-  // })),
+  withComputed(({ customersEntities, selectID }) => ({
+    selected: computed(() => customersEntities().filter((c) => c.id === selectID())),
+  })),
 
-  // withMethods((state) => {
-  //   const { basket, customersEntities, from, to, initialized } = state;
-  //   const flightService = inject(FlightService);
+  withMethods((state) => {
+    const { customersEntities, selectID, initialized } = state;
+    const customerService = inject(CustomerService);
 
-  //   return {
-  //     [...],
+    return {
+      updateSelectID: (selectID: number) => {
+        patchState(state, { selectID });
+      },
+    };
+  }),
 
-  //     load: async () => {
-  //       if (!from() || !to()) return;
-  //       patchState(state, setLoading());
+  withHooks({
+    onInit(store: any) {
 
-  //       const flights = await flightService.findPromise(from(), to());
+      effect(() => {
+        console.log('customerEntityMap', store.customerEntityMap())
+        // console.log('flightIds', store.flightIds())
+        // console.log('flightEntities', store.flightEntities())
+      });
 
-  //       // Updating entities with out-of-the-box setAllEntities Updater
-  //       patchState(state, setAllEntities(flights, { collection: 'flight' }));
-  //       patchState(state, setLoaded());
-  //     },
+    },
+    onDestroy() {
+      console.log('flights are destroyed');
+    },
+  }),
 
-  //     [...],
-  //   };
-  // }),
 );
